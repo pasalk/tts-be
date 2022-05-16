25653f0fa02c7ad1b42ddbe4661b6a7e8b00fbd3
psalkovic <paula.salkovic@biss.hr>
2022-05-16 17:59:03 +0200

    initial spring boot commit

---
+++ src/main/java/hr/fer/grad/proj/ttsapp/TtsAppApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.grad.proj.ttsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/fer/grad/proj/ttsapp/TtsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.grad.proj.ttsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
